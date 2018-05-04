cfa57d389ccffab26b417c157b0814257e7e91d6
chenjiankun <chenjiankun1@huawei.com>
2018-05-04 09:57:53 +0800

    start uwsgi

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@ cp docker/supervisor.conf /etc/supervisor/conf.d/
 # install nginx
 apt-get install -y nginx
 touch /var/run/publiccloud.sock
+chmod 666 /var/run/publiccloud.sock
 cp docker/publiccloud.conf /etc/nginx/conf.d/
 service nginx reload
 
@@ -14,7 +15,9 @@ service nginx reload
 apt-get install -y uwsgi
 mkdir -p /var/log/publiccloud
 
-
 # install python dependency
 apt-get install -y python-pip
 pip install -r requirements.txt
+
+# restart uwsgi
+uwsgi -i /home/publiccloud/docker/publiccloud.ini
