fb7e140cd28c8e52bd792cec25c9ffa09e6155f1
rexlee8776 <rexlee8776@gmail.com>
2018-05-26 04:01:05 +0000

    rename the project
    Signed-off-by: rexlee8776 <rexlee8776@gmail.com>

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
+SERVICE_NAME=yardstick-service
+YARDSTICK_SERVICE_HOME=/home/opnfv/repos/${SERVICE_NAME}
+
 rm /etc/nginx/conf.d/yardstick.conf
 #apt-get update
 
@@ -7,18 +10,19 @@ cp docker/supervisor.conf /etc/supervisor/conf.d/
 
 # install nginx
 # apt-get install -y nginx
-touch /var/run/publiccloud.sock
-chmod 666 /var/run/publiccloud.sock
-cp docker/publiccloud.conf /etc/nginx/conf.d/
+sock_file=/var/run/${SERVICE_NAME}.sock
+touch ${sock_file}
+chmod 666 ${sock_file}
+cp docker/${SERVICE_NAME}.conf /etc/nginx/conf.d/
 service nginx reload
 
 # install uwsgi
 # apt-get install -y uwsgi
-mkdir -p /var/log/publiccloud
+mkdir -p /var/log/${SERVICE_NAME}
 
 # install python dependency
 # apt-get install -y python-pip
 pip install -r requirements.txt
 
 # restart uwsgi
-uwsgi -i /home/publiccloud/docker/publiccloud.ini
+uwsgi -i ${YARDSTICK_SERVICE_HOME}/docker/${SERVICE_NAME}.ini
--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "publiccloud.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "yardstick-service.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
